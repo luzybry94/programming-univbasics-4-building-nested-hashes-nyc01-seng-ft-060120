94ae0b2645747eb6905299347874078175b5a299
Chaim Bryski <luzybry94@gmail.com>
2020-05-06 20:39:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,10 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+  railroads = {:railroads => {
+  }
+    
+  }
 
 end
 
@@ -9,6 +13,15 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   railroads = {:railroads => {  
+     :peices => 4,
+     :rent_in_dollars => {},
+     :names => {}
+
+  }
+    
+  }
+
 
 end
 
@@ -17,6 +30,25 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+   railroads = {:railroads => {  
+     :peices => 4,
+     :rent_in_dollars => {
+       :one_piece_owned => 25,
+       :two_pieces_owned => 50,
+       :three_peices_owned => 100
+       :four_pieces_owned => 200
+     },
+     :names => {
+       :reading_railroad => {},
+       :pennsylvania_railroad => {},
+       :b_and_o_railroad => {},
+       :shortline_railroad => {}
+       }
+     
+   }
+    
+  }
 
 end
 
@@ -25,5 +57,24 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+   railroads = {:railroads => {  
+     :peices => 4,
+     :rent_in_dollars => {
+       :one_piece_owned => 25,
+       :two_pieces_owned => 50,
+       :three_peices_owned => 100
+       :four_pieces_owned => 200
+     },
+     :names => {
+       :reading_railroad => {:mortgage_value => 100},
+       :pennsylvania_railroad => {:mortgage_value => 200},
+       :b_and_o_railroad => {:mortgage_value => 400},
+       :shortline_railroad => {:mortgage_value => 800}
+       }
+     
+   }
+    
+  }
 
 end
